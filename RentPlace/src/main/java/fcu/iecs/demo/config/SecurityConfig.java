bd0f62cfd109f585fcef7cc4f626d9a7589c8b11
呂雅慈 <161418704+D1204504@users.noreply.github.com>
2024-12-16 15:54:22 +0800

    更新HTML的CORS

--- RentPlace/src/main/java/fcu/iecs/demo/config/SecurityConfig.java
+++ RentPlace/src/main/java/fcu/iecs/demo/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     // 允許的來源（這裡是本地開發環境）
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:63342"));
     // 允許的 HTTP 方法
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     // 允許的 HTTP 標頭
