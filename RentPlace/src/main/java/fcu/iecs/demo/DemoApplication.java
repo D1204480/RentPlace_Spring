b434d7f679b9a028c5b5cb9963298a7153a24e33
D1204480 <161423607+D1204480@users.noreply.github.com>
2025-01-17 00:03:45 +0800

    Update DemoApplication.java

--- RentPlace/src/main/java/fcu/iecs/demo/DemoApplication.java
+++ RentPlace/src/main/java/fcu/iecs/demo/DemoApplication.java
@@ -1,10 +1,17 @@
 package fcu.iecs.demo;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @EnableScheduling
 @SpringBootApplication
 @EnableJpaRepositories
@@ -14,5 +21,21 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+	// 測試連線
+		@Autowired
+		private DataSource dataSource;
+
+		@PostConstruct
+		public void testConnection() {
+			try (Connection conn = dataSource.getConnection()) {
+				System.out.println("Database connection successful!");
+				System.out.println("Connected to: " + conn.getMetaData().getURL());
+			} catch (SQLException e) {
+				System.err.println("Database connection failed!");
+				e.printStackTrace();
+			}
+		}
 
 }
