025bb965accebcaea09e71ccabe695295be47793
D1204480 <161423607+D1204480@users.noreply.github.com>
2025-01-18 21:15:30 +0800

    update

--- RentPlace/src/main/java/fcu/iecs/demo/config/DatabaseConfig.java
+++ RentPlace/src/main/java/fcu/iecs/demo/config/DatabaseConfig.java
@@ -1,12 +1,19 @@
 package fcu.iecs.demo.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
@@ -25,6 +32,9 @@ public class DatabaseConfig {
   public DataSource dataSource() {
     HikariDataSource dataSource = new HikariDataSource();
 
+    // 打印出 URL 以進行調試（注意不要在生產環境打印敏感信息）
+    System.out.println("Database URL: " + url);
+
     // 使用 @Value 注入的值
     dataSource.setJdbcUrl(url);  // 使用 setJdbcUrl 而不是 setUrl
     dataSource.setUsername(username);
@@ -37,5 +47,29 @@ public class DatabaseConfig {
     dataSource.setConnectionTimeout(30000);
 
     return dataSource;
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+    em.setDataSource(dataSource());
+    em.setPackagesToScan("fcu.iecs.demo.model");  // 你的實體類包名
+
+    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    em.setJpaVendorAdapter(vendorAdapter);
+
+    Map<String, Object> properties = new HashMap<>();
+    properties.put("hibernate.hbm2ddl.auto", "update");
+    properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+    em.setJpaPropertyMap(properties);
+
+    return em;
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(emf);
+    return transactionManager;
   }
 }
