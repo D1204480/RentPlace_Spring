c0124c193aac4e434de8faf08f57219d18452c1a
D1204480 <161423607+D1204480@users.noreply.github.com>
2025-01-18 21:20:56 +0800

    update

--- RentPlace/src/main/java/fcu/iecs/demo/config/DatabaseConfig.java
+++ RentPlace/src/main/java/fcu/iecs/demo/config/DatabaseConfig.java
@@ -32,19 +32,24 @@ public class DatabaseConfig {
   public DataSource dataSource() {
     HikariDataSource dataSource = new HikariDataSource();
 
-    // 打印出 URL 以進行調試（注意不要在生產環境打印敏感信息）
-    System.out.println("Database URL: " + url);
-
-    // 使用 @Value 注入的值
-    dataSource.setJdbcUrl(url);  // 使用 setJdbcUrl 而不是 setUrl
+    dataSource.setJdbcUrl(url);
     dataSource.setUsername(username);
     dataSource.setPassword(password);
     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
-    // 設置連接池屬性
+    // 連接池設定
     dataSource.setMaximumPoolSize(5);
     dataSource.setMinimumIdle(2);
     dataSource.setConnectionTimeout(30000);
+    dataSource.setValidationTimeout(5000);
+
+    // 測試連接
+    dataSource.setConnectionTestQuery("SELECT 1");
+
+    // 添加額外的連接屬性
+    dataSource.addDataSourceProperty("allowPublicKeyRetrieval", "true");
+    dataSource.addDataSourceProperty("useSSL", "false");
+    dataSource.addDataSourceProperty("serverTimezone", "UTC");
 
     return dataSource;
   }
@@ -53,23 +58,19 @@ public class DatabaseConfig {
   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
     LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
     em.setDataSource(dataSource());
-    em.setPackagesToScan("fcu.iecs.demo.model");  // 你的實體類包名
+    em.setPackagesToScan("fcu.iecs.demo.model");
 
     HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    vendorAdapter.setGenerateDdl(true);
+    vendorAdapter.setShowSql(true);
     em.setJpaVendorAdapter(vendorAdapter);
 
     Map<String, Object> properties = new HashMap<>();
     properties.put("hibernate.hbm2ddl.auto", "update");
     properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+    properties.put("hibernate.globally_quoted_identifiers", "true");
     em.setJpaPropertyMap(properties);
 
     return em;
-  }
-
-  @Bean
-  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-    JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory(emf);
-    return transactionManager;
   }
 }
