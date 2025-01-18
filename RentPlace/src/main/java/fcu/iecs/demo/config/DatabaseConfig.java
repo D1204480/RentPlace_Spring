a0922317f2aac8388ba3441a5714cf0396c2cc3d
D1204480 <161423607+D1204480@users.noreply.github.com>
2025-01-18 21:07:46 +0800

    Update application.properties

--- RentPlace/src/main/java/fcu/iecs/demo/config/DatabaseConfig.java
+++ RentPlace/src/main/java/fcu/iecs/demo/config/DatabaseConfig.java
@@ -1,29 +1,34 @@
 package fcu.iecs.demo.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
+
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Configuration
 public class DatabaseConfig {
+
+  @Value("${spring.datasource.url}")
+  private String url;
+
+  @Value("${spring.datasource.username}")
+  private String username;
+
+  @Value("${spring.datasource.password}")
+  private String password;
 
   @Bean
   public DataSource dataSource() {
     HikariDataSource dataSource = new HikariDataSource();
 
-    // 使用環境變量
-    dataSource.setJdbcUrl(System.getenv("MYSQL_URL"));
-    dataSource.setUsername(System.getenv("MYSQLUSER"));
-    dataSource.setPassword(System.getenv("MYSQLPASSWORD"));
+    // 使用 @Value 注入的值
+    dataSource.setJdbcUrl(url);  // 使用 setJdbcUrl 而不是 setUrl
+    dataSource.setUsername(username);
+    dataSource.setPassword(password);
     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
     // 設置連接池屬性
@@ -32,29 +37,5 @@ public class DatabaseConfig {
     dataSource.setConnectionTimeout(30000);
 
     return dataSource;
-  }
-
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource(dataSource());
-    em.setPackagesToScan("fcu.iecs.demo.model");
-
-    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-    em.setJpaVendorAdapter(vendorAdapter);
-
-    Map<String, Object> properties = new HashMap<>();
-    properties.put("hibernate.hbm2ddl.auto", "update");
-    properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-    em.setJpaPropertyMap(properties);
-
-    return em;
-  }
-
-  @Bean
-  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-    JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory(emf);
-    return transactionManager;
   }
 }
